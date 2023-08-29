6ae6ccf6dff728683fa42ff8b64ba6b3dde7004e
iloveyoucow1 <42555537+iloveyoucow1@users.noreply.github.com>
2023-08-29 08:50:41 +0000

    Modified sort_cli.py: added no dir given and moved code to a function.

--- python/sort_cli.py
+++ python/sort_cli.py
@@ -1,20 +1,37 @@
+"""This module sorts a directory"""
 #!/usr/bin/python
 import sys
 import os
 
-directory = sys.argv[1]
-
-dir_list = os.listdir(directory)
-print(dir_list)
-
-for i in dir_list:
-    test = i.split(".")
-    if test[1] == "jpg":
-        os.mkdir(directory + test[1])
-
-
-
+#collects dir name given
 try:
-    directory_name=sys.argv[1]
-except:
-    print('Please pass directory_name') 
+    ARG_DIR=sys.argv[1]
+except IndexError:
+    print("ERROR: No Directory Given")
+    sys.exit()
+
+DIR_LIST = os.listdir(ARG_DIR)
+
+def __make_dir__(dirs):
+    ext_list = []
+
+    #makes a dir for every type of extension
+    for i in dirs:
+        ext = i.split(".")
+        try:
+            ext_list.append(ext[1])
+        except IndexError:
+            continue
+
+    ext_list = set(ext_list)
+    counter = 0
+    for i in ext_list:
+        try:
+            os.mkdir(ARG_DIR + i)
+            print("New dir made named: " + i)
+            counter += 1
+        except FileExistsError:
+            continue
+    return str(counter) + " Dir's made."
+
+print(__make_dir__(DIR_LIST))
